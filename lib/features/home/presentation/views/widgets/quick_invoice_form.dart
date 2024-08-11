801f8c988ad6a2d700fcdc09be8124d7e5cd59a2
Mohamed Gehad <mogehad2030@gmail.com>
2024-08-11 17:40:18 +0300

    Added Buttons to Quick Invoice form

--- lib/features/home/presentation/views/widgets/quick_invoice_form.dart
+++ lib/features/home/presentation/views/widgets/quick_invoice_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utlis/styles.dart';
 import 'textWithTextField.dart';
 
 class QuickInvoiceForm extends StatelessWidget {
@@ -9,34 +10,74 @@ class QuickInvoiceForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Row(
+        const Row(
           children: [
             Expanded(
                 child: TextWithTextField(
-                    txt: "Customer name",
-                    textField: "Type customer name")),
+                    txt: "Customer name", textField: "Type customer name")),
             SizedBox(width: 16),
             Expanded(
                 child: TextWithTextField(
-                    txt: "Customer Email",
-                    textField: "Type customer email")),
+                    txt: "Customer Email", textField: "Type customer email")),
           ],
         ),
-        SizedBox(height: 24),
-        Row(
+        const SizedBox(height: 24),
+        const Row(
           children: [
             Expanded(
                 child: TextWithTextField(
                     txt: "Item name", textField: "Type customer name")),
             SizedBox(width: 16),
             Expanded(
-                child: TextWithTextField(
-                    txt: "Item mount", textField: "USD")),
+                child: TextWithTextField(txt: "Item mount", textField: "USD")),
           ],
         ),
+        const SizedBox(height: 24),
+        Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                style: buildButtonStyle(),
+                onPressed: () {},
+                child: Text(
+                  "Add more details",
+                  style: Styles.textStyleSemiBold18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 24.0,
+            ),
+            Expanded(
+              child: TextButton(
+                style: buildButtonStyle().copyWith(
+                  backgroundColor:
+                      const WidgetStatePropertyAll(Color(0xff4EB7F2)),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Send Money",
+                  style:
+                      Styles.textStyleSemiBold18.copyWith(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        )
       ],
+    );
+  }
+
+  ButtonStyle buildButtonStyle() {
+    return ButtonStyle(
+      shape: WidgetStatePropertyAll(
+        ContinuousRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+      padding: const WidgetStatePropertyAll(EdgeInsets.all(24.0)),
     );
   }
 }
